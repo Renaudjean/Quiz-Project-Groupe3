1266974f8ad08afe0805fa51cdcb08c6cda45f97
RenaudJean <95225657+Renaudjean@users.noreply.github.com>
2022-03-23 08:29:42 +0100

    Server request on index

--- JS/server.js
+++ JS/server.js
@@ -1,8 +1,11 @@
 let createError = require('http-errors');
 let express = require('express');
+let router = express.Router();
 let path = require('path');
 let bodyParser = require('body-parser');
 let db= require('../database/database');
+const { Router } = require('express');
+const conn = require('../database/database');
 let app = express();
 let port = process.env.PORT || 5000;
 app.set('view engine', 'ejs');
@@ -13,11 +16,49 @@ app.use('/assets',express.static('public'));
 //mySQL
 
 
-//listen on environment 5000
+
 app.get('/', (req, response) => {
-    response.render("../index");
-    })
+    let row;
+    let holder;
+    let i =0;
+
+    let Quiz_Name = [], Quiz_Description= [], Quiz_Photo= [];
+
+    db.query('SELECT `Quiz_Name`, `Quiz_Description`, `Quiz_Photo` FROM `quiz`', function (err, row, fields){
+        if (err) throw err;
+        console.log(row);
+        response.render("../index", {quiz: row});
+                
+            })
+    
+    
+})
+
 app.get('/quiz', (req, response) => {
     response.render("../quiz");
     })
+
+    //listen on environment 5000
 app.listen(port, ()=> console.log(`listening on ${port}`));
+
+
+
+
+  // row.forEach(function(){
+            
+        //     db.query('SELECT `Quiz_Name` FROM `quiz`' , function (err, quiz_name, fields){
+        //     console.log(quiz_name[i]);
+        //     Quiz_Name.push(quiz_name[i]);
+        //      })
+        //      db.query('SELECT `Quiz_Description` FROM `quiz`' , function (err, quiz_description, fields){
+        //     console.log(quiz_description[i]);
+        //     Quiz_Description.push(quiz_description[i]);
+        //     })
+        //     db.query('SELECT `Quiz_Photo` FROM `quiz`' , function (err, quiz_photo, fields){
+            
+        //     Quiz_Photo.push(quiz_photo[i]);
+        //     console.log(Quiz_Photo[i]);
+        //         i++;     
+        //     })   
+            
+        // })
