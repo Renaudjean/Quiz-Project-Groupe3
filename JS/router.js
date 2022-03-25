765eb0567ee3e5e418be5f57f4173708343039b6
ericoriez <95495656+ericoriez@users.noreply.github.com>
2022-03-25 16:53:23 +0100

    login js

--- JS/router.js
+++ JS/router.js
@@ -4,12 +4,13 @@ const { Router } = require('express');
 const { route } = require('express/lib/router');
 let {quiz_Gen} = require('./app.js')
 let {question_Gen} = require('./api_questions.js')
-let {answer_Gen} = require('./api_answers.js')
+let {answer_Gen} = require('./api_answers.js');
+let {sign_check} = require('./api_login.js');
 
 router.get('/', quiz_Gen);
 router.get('/question/:id',question_Gen);
 router.get('/answer/:id',answer_Gen);
-router.get('/login/check', )
+router.get('/login/check',sign_check)
 
 
 module.exports = router;
