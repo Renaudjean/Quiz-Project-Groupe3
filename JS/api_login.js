5b176fc6f01da7e9805e399fbc2ea76398fb934a
RenaudJean <95225657+Renaudjean@users.noreply.github.com>
2022-03-29 08:57:44 +0200

    Reverting state

--- JS/api_login.js
+++ JS/api_login.js
@@ -1,10 +1,1 @@
 let db= require('../database/database');
-const conn = require('../database/database');
-
-
-module.exports.sign_check = (req, response) => {
-    db.query('SELECT `AC_ID`, `Email`, `UserName`, `FirstName`, `LastName`, `Password`, `AccountType` FROM `account`', [req.params.id], function (err, row, fields){
-        if (err) throw err;
-        response.json(row);       
-            })
-}
