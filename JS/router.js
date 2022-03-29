00ff8c06df7496569eafba634bc0827b68f905c4
RenaudJean <95225657+Renaudjean@users.noreply.github.com>
2022-03-29 09:07:08 +0200

    Revert "Merge branch 'Eric-branche'"
    This reverts commit 54a87f2293925bb37e46d91fcfea07eae71a8b25, reversing
    changes made to 58d21b3449891bb3b9d47e686ba998d6ac0e0dd0.

--- JS/router.js
+++ JS/router.js
@@ -6,8 +6,6 @@ let {quiz_Gen} = require('./app.js')
 let {question_Gen} = require('./api_questions.js')
 let {answer_Gen} = require('./api_answers.js')
 let {admin_Gen} = require('./api_adminhome.js')
-let {answer_Gen} = require('./api_answers.js');
-let {sign_check} = require('./api_login.js');
 
 router.get('/', quiz_Gen);
 router.get('/admin', admin_Gen);
@@ -16,7 +14,5 @@ router.get('/answer/:id',answer_Gen);
 router.get('/login/check', )
 router.get('/deletequiz/:id',);
 router.get('/correct-answer/:id',answer_Gen)
-router.get('/login/check',sign_check)
-
 
 module.exports = router;
