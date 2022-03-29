1f80449e113906c7c84096a72687e5ea3de4b8ba
RenaudJean <95225657+Renaudjean@users.noreply.github.com>
2022-03-29 09:06:34 +0200

    Revert "Update router.js"
    This reverts commit 871f6ca47b24f055b10b4ceece8b5c57c64fdcc8.

--- JS/router.js
+++ JS/router.js
@@ -5,14 +5,16 @@ const { route } = require('express/lib/router');
 let {quiz_Gen} = require('./app.js')
 let {question_Gen} = require('./api_questions.js')
 let {answer_Gen} = require('./api_answers.js')
+let {admin_Gen} = require('./api_adminhome.js')
 let {sign_check} = require('./api_login.js');
 
 router.get('/', quiz_Gen);
 router.get('/question/:id',question_Gen);
 router.get('/answer/:id',answer_Gen);
+router.get('/login/check', )
 router.get('/deletequiz/:id',);
 router.get('/correct-answer/:id',answer_Gen)
-// router.get('/login/check',sign_check)
+router.get('/login/check',sign_check)
 
 
 module.exports = router;
