52572ef1b25669fd49170ddf12bf88b1e52d0ac8
anastasiiaal <anastasia.alexeenko@gmail.com>
2022-03-31 14:26:47 +0200

    :crown: js | admin delete quiz works!
    :fireworks: congrats la team

--- public/script/admin.js
+++ public/script/admin.js
@@ -1,5 +1,7 @@
 const overlay = document.getElementById('overlay'),
-      modal = document.getElementById('modal-delete');
+      modal = document.getElementById('modal-delete'),
+      confirmBtn = document.getElementById('confirm-btn'),
+      mistakemBtn = document.getElementById('mistake-btn');
 
 let deleteThis = document.querySelectorAll('.btn-secondary.delete');
 
@@ -7,22 +9,35 @@ let quizOption;
 console.log(deleteThis);
 deleteThis.forEach(option => {
     option.addEventListener('click', (o) => {
+        // remove the default button action 
         o.preventDefault();
-
+        // call the modal and overlay
         overlay.classList.remove('dnone');
         modal.classList.remove('dnone');
-
-        quizOption = o.target;
-        let quizId= quizOption.dataset.id
-         
-        fetch('/deletethis/'+ quizId).then(res => {
-            fetch('/admin');
-        });
         
+        quizOption = o.target;
+        let quizId = quizOption.dataset.id;
+
+        console.log(quizOption);
+        confirmBtn.addEventListener('click', (a) => {
+            a.preventDefault();
+             
+            fetch('/deletethis/'+ quizId).then(res => {
+                fetch('/admin');
+            });
+            quizOption = '';
+            location.reload();
+        })
     })
 });
 
 overlay.addEventListener('click', () => {
-    overlay.classList.add('dnone');
-    modal.classList.add('dnone');
+    // overlay.classList.add('dnone');
+    // modal.classList.add('dnone');
+    // quizOption = '';
+    location.reload();
+})
+
+mistakemBtn.addEventListener('click', () => {
+    location.reload();
 })
