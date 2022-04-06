050edc26a6082c0fc9c26a248c681e43a9e6b84d
RenaudJean <95225657+Renaudjean@users.noreply.github.com>
2022-04-06 14:56:57 +0200

    Delete now deletes questions and answers

--- JS/api_deletethis.js
+++ JS/api_deletethis.js
@@ -2,4 +2,5 @@ let db= require('../database/database');
 
 module.exports.delete_This = (req, response) => {
     db.query('DELETE FROM `quiz` WHERE `Quiz_ID` = ?',[req.params.id])
+    db.query('DELETE Ques, Ans FROM `question` Ques INNER JOIN `Answer` Ans ON Ques.Question_ID = Ans.Question WHERE Ques.Quiz_ID = ?',[req.params.id])
 }
