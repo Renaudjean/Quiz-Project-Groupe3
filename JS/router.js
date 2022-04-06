41f2459b1c3b6dfab3027a94ea6d823e3731ddf4
ericoriez <95495656+ericoriez@users.noreply.github.com>
2022-04-06 15:40:18 +0200

    update

--- JS/router.js
+++ JS/router.js
@@ -14,7 +14,8 @@ let {photo_Gen} = require('./api_quiz.js');
 let {send_Scores}= require('./api_score.js');
 let {score_Collect}= require('./api_scorecollect.js');
 let {create_new_quiz}= require('./api_new_quiz');
-
+let {question_Update}= require('./api_question_update');
+let {answers_Update}= require('./api_answers_update');
 let {update_quiz}= require('./api_update.js');
 
 const { sign_logout } = require('./api_logout.js');
@@ -30,8 +31,9 @@ router.get('/login/up', sign_up_check_mail);
 router.get('/login/logout', sign_logout);
 router.post('/login/createUsers', sign_up_create_users);
 
-router.post('/update/', update_quiz);
-
+router.post('/update', update_quiz);
+router.post('/fetchQuest', question_Update);
+router.post('/fetchAnswers', answers_Update)
 router.get('/correct-answer/:id',answer_Gen);
 router.get('/quizz/:id', photo_Gen);
 router.get('/deletethis/:id', delete_This);
